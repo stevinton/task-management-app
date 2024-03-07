c329d442dfd805ab2c30a0e50491aa6032c51c65
stevinton <stevinton9@gmail.com>
2024-03-07 16:08:12 +0530

    completed

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ submit.addEventListener("click", function () {
     contentCreate();
     timer();
     createBox();
+    notifyMe();
 })
 
 const timer = () => {
@@ -144,3 +145,20 @@ const time = (data) =>{
 const notify = () => {
     new Notification("times up");
 }
+
+
+//extra
+function notifyMe() {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      const notification = new Notification("Task added");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification("Task added");
+        }
+      });
+    }
+  }
+  
